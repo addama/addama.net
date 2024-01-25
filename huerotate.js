e45239269ad255e193732ffdf9e187c0db993dcf
Addama <addama.sanders@gmail.com>
2024-01-25 15:32:12 -0800

    Arrived at a much better layout. Modified hue-rotate to change in 3 second intervals over a 2 second animation, rotating in a random direction each tick.

--- huerotate.js
+++ huerotate.js
@@ -1,15 +1,28 @@
-function hueRotate() {
-	let step = 0.5
-	let degree = document.documentElement.style.getPropertyValue('--hue-rotate') || '0deg';
-	degree = degree.substring(0, degree.length - 3);
-	if (+degree >= 358.5) {
-		degree = 0;
-	} else {
-		degree = +degree + step;
-	}
-	
-	document.documentElement.style.setProperty('--hue-rotate', degree + 'deg');
-	requestAnimationFrame(hueRotate);
+let last = 0
+const tickRate = 3000
+
+const r = (min, max) => {
+	// Random roll between min and max inclusive
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-requestAnimationFrame(hueRotate);
+function hueRotate(tick) {
+	if (!last || tick - last >= tickRate) {
+		last = tick
+		step = r(40,50) * (Math.random() < 0.5 ? -1 : 1)
+		let degree = document.documentElement.style.getPropertyValue('--hue-rotate') || '0deg'
+		degree = degree.substring(0, degree.length - 3);
+		//if (+degree >= (359 - 22.5) && step > 1) {
+		//	degree = (+degree + step) - 359
+		//} else if (+degree <= 22.5 && step < 1) {
+		//	degree = 359 + (+degree + step)
+		//} else {
+			degree = +degree + step
+		//}
+		
+		document.documentElement.style.setProperty('--hue-rotate', degree + 'deg')
+	}
+	requestAnimationFrame(hueRotate)
+}
+
+requestAnimationFrame(hueRotate)
