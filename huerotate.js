cce1f2162668bb90bb33adbc4396495892633c0e
Addama <addama.sanders@gmail.com>
2024-01-25 14:04:50 -0800

    Header changes to make sections more distinct, slowed down hue-rotate

--- huerotate.js
+++ huerotate.js
@@ -1,10 +1,11 @@
 function hueRotate() {
+	let step = 0.5
 	let degree = document.documentElement.style.getPropertyValue('--hue-rotate') || '0deg';
 	degree = degree.substring(0, degree.length - 3);
-	if (degree === 359) {
+	if (+degree >= 358.5) {
 		degree = 0;
 	} else {
-		degree ++;
+		degree = +degree + step;
 	}
 	
 	document.documentElement.style.setProperty('--hue-rotate', degree + 'deg');
